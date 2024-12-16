7eeaf77b2883a18d416e662c61034da8b09bcd84
iamakbarsha1 <iamakbarsha1@gmail.com>
2024-12-16 19:37:22 +0530

    Initial Commit

---
+++ deeds/deedsApp.swift
@@ -0,0 +1,17 @@
+//
+//  deedsApp.swift
+//  deeds
+//
+//  Created by Akbarsha on 16/12/24.
+//
+
+import SwiftUI
+
+@main
+struct deedsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ deedsTests/deedsTests.swift
@@ -0,0 +1,17 @@
+//
+//  deedsTests.swift
+//  deedsTests
+//
+//  Created by Akbarsha on 16/12/24.
+//
+
+import Testing
+@testable import deeds
+
+struct deedsTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
